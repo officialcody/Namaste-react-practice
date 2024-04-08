889dd455d1e9cad3121c206ac1e78a442d47b365
Sparsh Goel <sparshgoelt26@gmail.com>
2024-04-08 15:14:46 +0530

    Initialize

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement(
+  "div",
+  { id: "heading" },
+  "I am an element"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
