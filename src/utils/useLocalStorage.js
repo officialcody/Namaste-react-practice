0cd938027d59a463a0661ebd70da899f5c223ccc
Sparsh Goel <sparshgoelt26@gmail.com>
2024-05-01 19:56:37 +0530

    useLocalStorage hook

---
+++ src/utils/useLocalStorage.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+const useLocalStorage = () => {
+  const setItem = (key, value) => {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+  const getItem = (key) => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : undefined;
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+  const removeItem = (key) => {
+    try {
+      window.localStorage.removeItem(key);
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+  const clear = () => {
+    try {
+      window.localStorage.clear();
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+  return [setItem, getItem, removeItem, clear];
+};
+
+export default useLocalStorage;
