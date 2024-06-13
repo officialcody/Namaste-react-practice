5ee68d559db2761a21b4854815ab189bd620898c
Sparsh Goel <sparshgoelt26@gmail.com>
2024-06-13 15:42:15 +0530

    CartPage design changes

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,20 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import CartItem from "./CartItem";
+import { clearCart } from "../utils/slices/CartSlice";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
+
+  const dispatch = useDispatch();
+
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+
+  const getTotalPrice = () => {
+    return cartItems.reduce((acc, curr) => acc + curr.price, 0);
+  };
 
   if (cartItems.length === 0) {
     return (
@@ -12,10 +23,26 @@ const Cart = () => {
   }
 
   return (
-    <div className="flex">
-      {cartItems.map((item) => (
-        <CartItem key={item.id} {...item} />
-      ))}
+    <div className="flex flex-col text-center">
+      <div>
+        <h1 className="text-4xl font-bold p-2 m-2">Cart</h1>
+        <button
+          className="btn rounded-lg bg-red-500 p-2 m-2 text-white"
+          onClick={handleClearCart}
+        >
+          Clear Cart
+        </button>
+      </div>
+      <ul className="max-w-md divide-y py-auto mx-auto divide-gray-200 dark:divide-gray-700">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} {...item} />
+        ))}
+      </ul>
+      <div className="border-b border-t border-bold text-bold p-4 m-4">
+        <h1 className="text-4xl p-2 m-2">
+          Total Price: ₹{getTotalPrice() / 100}.00
+        </h1>
+      </div>
     </div>
   );
 };
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,12 +1,28 @@
 import { IMG_CDN_URL } from "../utils/app.constants.js";
 const CartItem = ({ name, description, price, imageId }) => {
   return (
-    <div className="m-4 p-4 w-[250px] min-h-[470px] bg-gray-100 rounded-lg">
-      <img className="rounded-lg" src={IMG_CDN_URL + imageId} />
-      <h2 className="font-bold py-4 text-lg">{name}</h2>
-      <h3>{description}</h3>
-      <h4>Price: {parseFloat(price / 100)}</h4>
-    </div>
+    <li className="pb-3 sm:pb-4">
+      <div className="flex items-center space-x-4 rtl:space-x-reverse">
+        <div className="flex-shrink-0">
+          <img
+            className="w-24 h-24 rounded-full"
+            src={IMG_CDN_URL + imageId}
+            alt="image not found"
+          />
+        </div>
+        <div className="grid justify-items-stretch flex-1 min-w-0">
+          <p className="text-sm p-2 m-2 font-bold text-gray-900 justify-self-center truncate">
+            {name}
+          </p>
+          <p className="text-sm text-gray-500 text-wrap truncate items">
+            {description}
+          </p>
+        </div>
+        <div className="inline-flex items-center text-base font-bold text-gray-900">
+          Price: ₹{parseFloat(price / 100)}.00
+        </div>
+      </div>
+    </li>
   );
 };
 export default CartItem;
